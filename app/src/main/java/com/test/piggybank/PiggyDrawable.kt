46817b3e1f62a413e7b2a40e64161655f8699c2d
Maxwell Talbot <talbot.maxwell@gmail.com>
2021-10-05 10:59:07 +0100

    added some more comments to the Drawable

--- app/src/main/java/com/test/piggybank/PiggyDrawable.kt
+++ app/src/main/java/com/test/piggybank/PiggyDrawable.kt
@@ -31,24 +31,56 @@ class PiggyDrawable(piggyFile: File) : Drawable() {
         piggyStateMachineInstance = StateMachineInstance(piggyStateMachine)
         pressedInput = piggyStateMachineInstance.input("Pressed") as SMITrigger
 
+        // We are using an animator to continously advance our animation
         animator.setTimeListener { _, _, delta ->
             advance(delta.toFloat())
         }
         animator.start()
     }
 
+    /**
+     * Add a new coin into the mix.
+     */
     fun showMeTheMoney() {
         pressedInput.fire()
+
+        // create a new state machine instance for the coin state machine, so we can track
+        // its state separately to the other flying coins.
         val coinStateMachineInstance = StateMachineInstance(coinStateMachine);
+
+        // randomize the state machine input slightly so that the coins do not all follow the same path
         val coinRandomInput = coinStateMachineInstance.input("CoinRandomization") as SMINumber;
         coinRandomInput.value = (Math.random() * 100).toFloat();
-        val newPair = Pair(coinArtboard.getInstance(), coinStateMachineInstance)
-        newPair.second.advance(newPair.first, 0.0f);
-        newPair.first.advance(0.0f);
+
+        // instantiate a fresh copy of the coinArtboard, this creates a new coin shape that we can
+        // move and animate, without impacting other flying coins
+        val coinArtboardInstance = coinArtboard.getInstance()
+
+        // its time advance our coin, it forces the beginning key of the animation to be applied.
+        // without this, the first frame would be how the artboard looks in design mode, in this case
+        // a coin in the center of the artbaord.
+        coinStateMachineInstance.advance(coinArtboardInstance, 0.0f);
+        coinArtboardInstance.advance(0.0f);
+
+        // track this pair so that we can advance the animation and discard it when the animation
+        // is over.
+        val newPair = Pair(coinArtboardInstance, coinStateMachineInstance)
         flyingCoins.add(newPair)
         flyingCoinTimes[newPair] = 0.0f
     }
 
+    /**
+     * Advance the animation by some time.
+     *
+     * This is the crux of running rive animations, we apply the elapsed time to
+     * all active animations/ state machines.
+     *
+     * We know our flying coin animation lasts about 1.7 seconds.
+     * So we remove it once that amount of time has elapsed.
+     *
+     * And then we call "invalidateSelf" which tells android that this object needs to be drawn.
+     *
+     */
     fun advance(delta: Float) {
         val elapsed = delta / 1000
         backgroundArtboard.advance(elapsed)
@@ -73,6 +105,16 @@ class PiggyDrawable(piggyFile: File) : Drawable() {
         invalidateSelf()
     }
 
+    /**
+     * Draw our animation in its current state when android requests a draw operation.
+     *
+     * 1. Bind the canvas to our low level renderer
+     *  - the renderer will run draw operations against the canvas when requested.
+     * 2. Align the renderer, so it knows how to fit the artboard draw operations onto the canvas
+     * 3. Draw the background
+     * 4. Draw any flying coins
+     * 5. Draw the pig
+     */
     override fun draw(canvas: Canvas) {
         renderer.canvas = canvas
 
@@ -92,6 +134,7 @@ class PiggyDrawable(piggyFile: File) : Drawable() {
         canvas.restoreToCount(saved)
     }
 
+    // some required overrides that we have not paid attention to for this example.
     override fun setAlpha(alpha: Int) {}
     override fun setColorFilter(colorFilter: ColorFilter?) {}
     override fun getOpacity(): Int {
